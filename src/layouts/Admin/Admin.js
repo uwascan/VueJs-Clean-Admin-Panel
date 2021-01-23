91d9f6d320990bc324cb2f2b78eb501b30e474c8
Gentrit Abazi <gentritabazi01@gmail.com>
2021-01-23 23:01:45 +0100

    Add bootstrap.

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -1,22 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 export default {
-    name: 'AdminLayout',
-    created() {
-        let popper = document.createElement('script');
-        popper.setAttribute('src', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
-
-        let bootstrapJs = document.createElement('script');
-        bootstrapJs.setAttribute('src', 'https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js');
-        
-        let bootstrapCss = document.createElement('link');
-        bootstrapCss.setAttribute('href', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css');
-        bootstrapCss.setAttribute('rel', 'stylesheet');
-
-        let fatherIconsJs = document.createElement('script');
-        fatherIconsJs.setAttribute('src', 'https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js');
-
-        document.head.appendChild(popper);
-        document.head.appendChild(bootstrapJs);
-        document.head.appendChild(bootstrapCss);
-        document.head.appendChild(fatherIconsJs);
-    }
+    name: 'AdminLayout'
 }
