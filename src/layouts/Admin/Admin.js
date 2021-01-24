6f63701e7fa293c3e962c3f80b34592ea4680ab9
Gentrit Abazi <gentritabazi01@gmail.com>
2021-01-24 15:24:06 +0100

    Improve Style.

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 export default {
     name: 'AdminLayout'
